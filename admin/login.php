6534ecb1e76838b0b45898f8315a1ae43e741935
Jari <zalmanhajari@gmail.com>
2022-08-03 00:02:25 +0800

    Fixed the issue of login and register pages's fonts not getting changed

--- admin/login.php
+++ admin/login.php
@@ -56,7 +56,7 @@ if (isset($_POST['login'])) {
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	
+	<link href='https://fonts.googleapis.com/css?family=Source Sans Pro' rel='stylesheet'>
 	<style>
 		body,
 		html {
