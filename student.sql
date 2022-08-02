0e98c67b76852ffaeb012df41699304e8ec03703
Asher <Ujjwal>
2022-08-03 07:30:53 +0800

    new server

--- student.sql
+++ student.sql
@@ -6,7 +6,7 @@
 -- Generation Time: Dec 06, 2020 at 02:22 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
-
+ 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
